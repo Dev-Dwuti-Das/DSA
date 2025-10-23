9d2b55716dc5afff2ac776f4d144fdea668eedd4
Dev Dwuti Das <devdwuti@gmail.com>
2025-10-23 12:12:14 +0530

    Time: 5 ms (3.31%) | Memory: 56.2 MB (23.51%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -7,11 +7,9 @@ class Solution {
         suffix[nums.length-1] = nums[nums.length-1];
         for(int i = 1; i < nums.length; i++){
             prefix[i] = prefix[i-1] * nums[i]; 
-           System.out.print(prefix[i] + " ");
         }
         for(int i = nums.length-2; i > 0 ; i--){
             suffix[i] = suffix[i+1] * nums[i]; 
-            // System.out.print(suffix[i] + " ");
         }
         for(int i = 0; i < nums.length; i++){
             if(i == 0){
