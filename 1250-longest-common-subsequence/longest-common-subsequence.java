2b9570d0cbfcf422007b84d5da18ec3d3f0614f1
Dev Dwuti Das <devdwuti@gmail.com>
2025-11-07 13:04:13 +0530

    Time: 25 ms (27.53%) | Memory: 53.9 MB (8.49%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -5,7 +5,6 @@ class Solution {
 
         int[][] dp = new int[n][m]; 
 
-        // initialize dp with -1 (means not computed)
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 dp[i][j] = -1;
@@ -17,18 +16,16 @@ class Solution {
 
     public int solve(int i, int j, String s1, String s2, int[][] dp){
 
-        // Base Case
+
         if(i < 0 || j < 0) return 0;
 
-        // If already computed
+
         if(dp[i][j] != -1) return dp[i][j];
 
-        // Match
         if(s1.charAt(i) == s2.charAt(j)){
             return dp[i][j] = 1 + solve(i-1, j-1, s1, s2, dp);
         }
 
-        // No Match → take max
         return dp[i][j] = Math.max(
             solve(i-1, j, s1, s2, dp),
             solve(i, j-1, s1, s2, dp)
