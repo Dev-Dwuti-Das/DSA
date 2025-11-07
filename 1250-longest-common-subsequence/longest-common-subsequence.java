9224d5529e6d0ff013300cfd0eef425c49eaab6b
Dev Dwuti Das <devdwuti@gmail.com>
2025-11-07 13:03:31 +0530

    Time: 24 ms (32.11%) | Memory: 54 MB (8.49%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,35 +1,37 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int idx1 = text1.length();
-        int idx2 = text2.length();
-        ArrayList<ArrayList<Integer>> dp = new ArrayList<>();
-        for(int i = 0; i < text1.length(); i++){
-            dp.add(new ArrayList<>());
-            for(int j = 0 ; j < text2.length(); j++){
-                dp.get(i).add(-1);
+        int n = text1.length();
+        int m = text2.length();
+
+        int[][] dp = new int[n][m]; 
+
+        // initialize dp with -1 (means not computed)
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                dp[i][j] = -1;
             }
         }
-        return customLcs(idx1-1, idx2-1, text1, text2 ,dp);
-        
+
+        return solve(n-1, m-1, text1, text2, dp);
     }
 
-    public int customLcs(int id1, int id2, String s1, String s2, ArrayList<ArrayList<Integer>> dp){
-        int count;
-        if(id1 < 0 || id2 < 0){
-            return 0;
+    public int solve(int i, int j, String s1, String s2, int[][] dp){
+
+        // Base Case
+        if(i < 0 || j < 0) return 0;
+
+        // If already computed
+        if(dp[i][j] != -1) return dp[i][j];
+
+        // Match
+        if(s1.charAt(i) == s2.charAt(j)){
+            return dp[i][j] = 1 + solve(i-1, j-1, s1, s2, dp);
         }
 
-        if(dp.get(id1).get(id2) != -1){
-            return dp.get(id1).get(id2);
-        }
-
-        if(s1.charAt(id1) == s2.charAt(id2)){
-            int val =  1 + customLcs(id1-1, id2-1,s1, s2,dp);
-            dp.get(id1).set(id2,val);
-            return val;
-        }
-        int val = Math.max(customLcs(id1, id2-1, s1, s2, dp),customLcs(id1-1, id2, s1, s2, dp)); 
-        dp.get(id1).set(id2, val);
-        return val;  
+        // No Match → take max
+        return dp[i][j] = Math.max(
+            solve(i-1, j, s1, s2, dp),
+            solve(i, j-1, s1, s2, dp)
+        );
     }
 }
