0200ddfe64f7f8035cea154659c27263bd4639c9
Dev Dwuti Das <devdwuti@gmail.com>
2025-12-02 17:18:44 +0530

    Time: 17 ms (5.20%) | Memory: 45.5 MB (9.48%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+
+        String[] paths = path.split("/");
+
+        for (String c : paths) {
+            if (c.equals("..")) {
+                if (!stack.isEmpty())
+                    stack.pop();
+            } else if (!c.equals(".") && !c.equals("")) {
+                stack.push(c);
+            }
+        }
+        for (String s : stack) {
+            System.out.print(s);
+        }
+
+        return "/" + String.join("/", stack);
+    }
+}
+
+//             boolean flag = false;
+//             if(!stack.isEmpty() && c == '/' && stack.peek() == '/'){
+//                 flag = true;
+//             }
+//             if(!flag){
+//                 stack.push(c);
+//             }
+//         }
+//         if(stack.peek() == '/'){
+//             stack.pop();
+//         }
+//         
+//         return sb.toString();
+//     }
+// }
