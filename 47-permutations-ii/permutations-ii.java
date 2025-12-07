57b9557a59a43d1186d87989b536245535a5bf37
Dev Dwuti Das <devdwuti@gmail.com>
2025-12-07 23:45:46 +0530

    Time: 3 ms (66.60%) | Memory: 46.6 MB (29.83%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        boolean[] bool = new boolean[nums.length];
+        Arrays.sort(nums);
+        ArrayList<Integer> temp = new ArrayList<>();
+        ArrayList<List<Integer>> res= new ArrayList<>();
+        helper(nums, bool, temp, res);
+        return res;
+
+    }
+
+    public void helper(int[] nums, boolean[] bool, ArrayList<Integer> temp, ArrayList<List<Integer>> res) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if (bool[j]) continue;
+            if ( j>0 && nums[j] == nums[j - 1 ] && !bool[j-1]) { //bhara hua h to 
+                continue;
+            }
+            //khali
+            bool[j] = true;
+            temp.add(nums[j]);
+            helper(nums, bool, temp, res);
+            bool[j] = false;
+            temp.remove(temp.size() - 1);
+        }
+
+    }
+}
