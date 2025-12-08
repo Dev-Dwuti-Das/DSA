3e52de0fb2140bbfab946dc18a1f4bc8a8a722aa
Dev Dwuti Das <devdwuti@gmail.com>
2025-12-08 19:19:41 +0530

    Time: 1 ms (99.89%) | Memory: 48.3 MB (56.02%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,37 +1,39 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        int n = len1+len2; 
-        if(len1 > len2){
-            return findMedianSortedArrays(nums2, nums1);
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if(n1 > n2){
+           return findMedianSortedArrays(nums2 , nums1);
         }
-        int left = 0;
-        int right = len1;
 
-        while(left <= right){ 
-            int mid1 = (right+left)/2;
-            int mid2 = (n+1)/2 - mid1;
+        int min = 0;
+        int max = n1;
+        int left = (n1 + n2 + 1)/2;
 
-            int leftmax1 = (mid1 == 0)? Integer.MIN_VALUE : nums1[mid1-1];
-            int leftmax2 = (mid2 == 0)? Integer.MIN_VALUE : nums2[mid2-1];
-            int rightmin1 = (mid1 == len1)? Integer.MAX_VALUE :nums1[mid1];
-            int rightmin2 = (mid2 == len2)? Integer.MAX_VALUE :nums2[mid2];
+        while(min <= max){
+            int mid1 = min + (max - min)/2;
+            int mid2 = left - mid1;
 
-            if(leftmax1 <= rightmin2 && leftmax2 <= rightmin1){
-                if(n%2 == 0){
-                    return (Math.max(leftmax1, leftmax2)+Math.min(rightmin1, rightmin2))/2.0;
+            int l1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[mid1-1];
+            int l2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[mid2-1];
+            int r1 = (mid1 == n1) ? Integer.MAX_VALUE : nums1[mid1];
+            int r2 = (mid2 == n2) ? Integer.MAX_VALUE : nums2[mid2];
+            
+
+            if(l1 <= r2 && l2 <= r1){
+                if((n1+n2)%2 == 0){
+                    return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+                }else{
+                    return Math.max(l1,l2);
                 }
-                else{
-                    return (double)Math.max(leftmax1, leftmax2);
-                }
-            }else if(leftmax1 > rightmin2){
-                right = mid1-1;
+
+            }else if( l1 > r2){
+                max = mid1 - 1; 
+            }else{
+                min = mid1 + 1;
+
             }
-            else{
-                left = mid1+1;
-            }
-        }
-        return 0.0; 
+        } 
+    return 0.0;
     }
 }
