c771d8de4e963c008e6d2c7181def60fcec7c556
Dev Dwuti Das <devdwuti@gmail.com>
2025-12-14 21:33:29 +0530

    Time: 1 ms (83.55%) | Memory: 44.2 MB (59.16%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,22 +1,27 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<>();
-        helper(n, res, 0 , 0, "");
-        return res;
+    StringBuilder sb = new StringBuilder();
+    List<String> res = new ArrayList<>();
+    backtrack(n, sb, 0, 0, res);
+    return res;
     }
 
-    public void helper(int n, List<String> res, int open, int close, String st){
-        if(st.length() == 2*n){
-            res.add(st);
+    public void backtrack(int n, StringBuilder sb, int open, int close, List<String> res){
+        if(sb.length() ==  2*n){
+            res.add(sb.toString());
             return;
         }
 
-        if( open < n){
-            helper(n, res, open+1 , close, st+"(");
+        if(open < n){
+            sb.append("(");
+            backtrack(n, sb, open+1, close, res);
+            sb.deleteCharAt(sb.length()-1);
         }
 
-        if( close < open){
-            helper(n, res, open , close+1, st+")");
+        if(close < open){
+            sb.append(")");
+            backtrack(n, sb, open, close+1, res);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }
