42d88c380b19f3ad8d102bf1d3fe6234931ac924
Dev Dwuti Das <devdwuti@gmail.com>
2025-12-24 13:31:55 +0530

    Time: 343 ms (16.79%) | Memory: 60.6 MB (5.22%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,33 +1,29 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List <List<Integer>> result = new ArrayList<>();
-        Set <List<Integer>> set = new HashSet<>();
-
-        for(int i = 0; i < nums.length-2; i++){
-            int l = i+1;
-            int r = nums.length-1;
-            int pivot = nums[i];
-            while(l < r){
-                if(pivot + nums[l] + nums[r] == 0){
-                    List <Integer> temp = new ArrayList<>();
-                    temp.add(pivot);
-                    temp.add(nums[l]);
-                    temp.add(nums[r]);
-                    if(set.add(temp)){
-                        result.add(temp);
-                    }
-                    l++;
-                    r--;
-                }
-                else if(pivot + nums[l] + nums[r] < 0 ){
-                    l++;
-                }
-                else{
-                    r--;
-                }
-            }
+  public List<List<Integer>> threeSum(int[] nums) {
+    Arrays.sort(nums);
+    List<List<Integer>> res = new ArrayList<>();
+    Set<List<Integer>> set = new HashSet<>();
+    for (int pivot = 0; pivot < nums.length - 2; pivot++) {
+      int l = pivot + 1;
+      int r = nums.length - 1;
+      while (l < r) {
+        if (nums[l] + nums[r] + nums[pivot] == 0) {
+          List<Integer> temp = new ArrayList<>();
+          temp.add(nums[l]);
+          temp.add(nums[r]);
+          temp.add(nums[pivot]);
+          if (set.add(temp)) {
+            res.add(temp);
+          }
+          l++;
+          r--; //yeah yaaad kari
+        } else if (nums[l] + nums[r] + nums[pivot] > 0) {
+          r--;
+        } else {
+          l++;
         }
-    return result;  
+      }
     }
+    return res;
+  }
 }
