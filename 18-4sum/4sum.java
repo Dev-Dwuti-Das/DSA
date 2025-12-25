67dfe114785d48fbef95944f89eab8dbf9fd1aea
Dev Dwuti Das <devdwuti@gmail.com>
2025-12-25 11:01:52 +0530

    Time: 21 ms (37.40%) | Memory: 45.7 MB (68.83%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,35 +1,35 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
+  public List<List<Integer>> fourSum(int[] nums, int target) {
+    List<List<Integer>> res = new ArrayList<>();
+    Arrays.sort(nums);
+    int n = nums.length;
+    for (int p1 = 0; p1 < n - 3; p1++) {
+      if (p1 > 0 && nums[p1 - 1] == nums[p1])
+        continue;
+      for (int p2 = n - 1; p2 > p1 + 2; p2--) {
+        if (p2 < n - 1 && nums[p2 + 1] == nums[p2])
+          continue;
+        int l = p1 + 1;
+        int r = p2 - 1;
+        while (l < r) {
+          long sum_4 = (long)nums[p1] + nums[p2] + nums[l] + nums[r];
+          if (sum_4 == target) {
+            res.add(Arrays.asList(nums[p1], nums[l], nums[r], nums[p2]));
 
-        for (int p1 = 0; p1 < n - 3; p1++) {
-            if (p1 > 0 && nums[p1] == nums[p1 - 1]) continue;
-
-            for (int p2 = n - 1; p2 > p1 + 2; p2--) {
-                if (p2 < n - 1 && nums[p2] == nums[p2 + 1]) continue;
-
-                int l = p1 + 1;
-                int r = p2 - 1;
-
-                while (l < r) {
-                    long sum = (long) nums[p1] + nums[l] + nums[r] + nums[p2];
-
-                    if (sum == target) {
-                        res.add(Arrays.asList(nums[p1], nums[l], nums[r], nums[p2]));
-                        l++;
-                        r--;
-                        while (l < r && nums[l] == nums[l - 1]) l++;
-                        while (l < r && nums[r] == nums[r + 1]) r--;
-                    } else if (sum < target) {
-                        l++;
-                    } else {
-                        r--;
-                    }
-                }
-            }
+            r--;
+            l++;
+            while (l < r && nums[l - 1] == nums[l])
+              l++;
+            while (l < r && nums[r + 1] == nums[r])
+              r--;
+          } else if (sum_4 > target) {
+            r--;
+          } else {
+            l++;
+          }
         }
-        return res;
+      }
     }
+    return res;
+  }
 }
