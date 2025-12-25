b03bee75c4af6e529f3c6725d4d5912d912b4312
Dev Dwuti Das <devdwuti@gmail.com>
2025-12-25 12:05:05 +0530

    Time: 1 ms (1.72%) | Memory: 42.8 MB (40.52%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -20,7 +20,7 @@ class Solution {
 
       if((p == null && q!= null)||(p!=null && q == null)) return false;
 
-      if(p!= null && q!= null & p.val != q.val) return false;
+      if(p.val != q.val) return false;
       
       isSameTree(p.left, q.left);
       isSameTree(p.right, q.right);
