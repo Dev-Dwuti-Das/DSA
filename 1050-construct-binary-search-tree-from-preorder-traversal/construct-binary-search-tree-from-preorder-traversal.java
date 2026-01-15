3dcbb35ba2fcecb2c8c5f79617112c08ba85df34
Dev Dwuti Das <devdwuti@gmail.com>
2026-01-15 17:05:47 +0530

    Time: 1 ms (14.08%) | Memory: 43.5 MB (60.49%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -1,35 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
-    int idx = 0;
-
+  int idx= 0;
     public TreeNode bstFromPreorder(int[] preorder) {
-
-        TreeNode root = new TreeNode(preorder[idx++]); // first element as root
-        helper(preorder, Integer.MAX_VALUE, root);
-        return root;
+      TreeNode root = new TreeNode(preorder[idx++]);
+      helper(preorder, Integer.MAX_VALUE, root);
+      return root;
     }
 
-    public void helper(int[] preorder, int UB, TreeNode root) {
 
-        if (root == null || idx >= preorder.length) {
-            return;
-        }
+    public void helper(int[] preorder, int UB,  TreeNode root){
+      if( root == null || idx >= preorder.length ) return;
 
-        // LEFT child
-        if (idx < preorder.length && preorder[idx] < root.val) {
-            root.left = new TreeNode(preorder[idx++]);
-            helper(preorder, root.val, root.left);
-        } else {
-            root.left = null;
-        }
+      if(idx < preorder.length && preorder[idx] < root.val ){
+        root.left = new TreeNode(preorder[idx++]);
+        helper(preorder, root.val, root.left);
+      }
+      else{
+        root.left = null;
+      }
 
-        // RIGHT child
-        if (idx < preorder.length && preorder[idx] < UB) {
-            root.right = new TreeNode(preorder[idx++]);
-            helper(preorder, UB, root.right);
-        } else {
-            root.right = null;
-        }
-   
+      if(idx < preorder.length && preorder[idx] > root.val && preorder[idx] < UB){
+        root.right = new TreeNode(preorder[idx++]);
+        helper(preorder, UB, root.right);
+      }
+      else{
+        root.right = null;
+      }      
     }
 }
