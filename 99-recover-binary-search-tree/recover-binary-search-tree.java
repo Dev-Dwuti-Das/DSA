28c064ff0ff590ac1dcaef4c2ea2a334f3345e85
Dev Dwuti Das <devdwuti@gmail.com>
2026-01-17 09:12:18 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (16.92%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  TreeNode first;
+  TreeNode mid;
+  TreeNode last;
+  TreeNode prev;
+
+  public void inorder(TreeNode root) {
+    if (root == null)
+      return;
+    inorder(root.left);
+    if (prev.val > root.val) {
+      if (first == null) {
+        first = prev;
+        mid = root;
+      } else {
+        last = root;
+      }
+    }
+    prev = root;
+    inorder(root.right);
+  }
+
+  public void recoverTree(TreeNode root) {
+    if (root == null)
+      return;
+    first = mid = last = null;
+    prev = new TreeNode(Integer.MIN_VALUE);
+    inorder(root);
+    if (first != null && last != null) {
+      int temp = last.val;
+      last.val = first.val;
+      first.val = temp;
+    } else if(first != null && mid !=null){
+      int temp = mid.val;
+      mid.val = first.val;
+      first.val = temp;
+    }
+  }
+}
