c9cb749b1ade171f555ffd1b7ff39ecb0528e5cb
Dev Dwuti Das <devdwuti@gmail.com>
2026-02-14 18:38:49 +0530

    Time: 4 ms (1.75%) | Memory: 47 MB (24.06%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,27 +1,36 @@
 class Solution {
-  public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-    int m = image.length;
-    int n = image[0].length;
-    int currcolor = image[sr][sc];
-    boolean[][] isvalid = new boolean[m][n];
-    if (image[sr][sc] != color) {
-      dfs(sr, sc, image, color, currcolor );
-    }
-    return image;
-  }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n = image.length;
+        int m = image[0].length;
+        boolean[][] visited = new boolean[n][m];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] { sr, sc });
+        visited[sr][sc] = true;
+        int curcol = image[sr][sc];
+        image[sr][sc] = color;
+        System.out.print(image[sr][sc]);
 
-  public void dfs(int row, int col, int[][] image, int color, int cc) {
-    if (row < 0 || col < 0 || row >= image.length || col >= image[0].length) {
-      return;
-    }
-    if( image[row][col] == color || image[row][col] != cc){
-      return;
-    }
+        int[][] dir = {
+                { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 }
+        };
 
-    image[row][col] = color;
-    dfs(row + 1, col, image, color, cc);
-    dfs(row - 1, col, image, color, cc);
-    dfs(row, col + 1, image, color, cc);
-    dfs(row, col - 1, image, color, cc);
-  }
+        while (!q.isEmpty()) {
+            int[] item = q.poll();
+            int r = item[0];
+            int c = item[1];
+
+            for (int[] d : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr < n && nc < m && nc >= 0 && nr >= 0 && !visited[nr][nc] && image[nr][nc] == curcol) {
+                    q.offer(new int[] { nr, nc});
+                    visited[nr][nc] = true;
+                    image[nr][nc] = color;
+                }
+            }
+
+        }
+        return image;
+    }
 }
