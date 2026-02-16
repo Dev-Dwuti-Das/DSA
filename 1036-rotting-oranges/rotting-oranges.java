bed851da60959b2c68e86cf9aaa51ba6b95d87f5
Dev Dwuti Das <devdwuti@gmail.com>
2026-02-16 09:37:03 +0530

    Time: 2 ms (90.88%) | Memory: 44 MB (84.79%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,48 +1,56 @@
 class Solution {
-  public int orangesRotting(int[][] grid) {
-    int time = 0;
-    int fresh = 0;
-    int n = grid.length;
-    int m = grid[0].length;
-    Queue<int[]> q = new LinkedList<>();
-    boolean[][] isvisited = new boolean[n][m];
-    int[] rowdir = { -1, +1, 0, 0 };
-    int[] coldir = { 0, 0, -1, +1 };
-    for (int i = 0; i < grid.length; i++) {
-      for (int j = 0; j < grid[0].length; j++) {
-        if (grid[i][j] == 2) {
-          q.offer(new int[]{i,j});  
-          isvisited[i][j] = true;
+    public int orangesRotting(int[][] grid) {
+        int fresh = 0;
+        int time = 0;
+        int[][] dir = {
+                { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 }
+        };
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2) {
+                    q.offer(new int[] { i, j });
+                }
+                if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
         }
-        else if(grid[i][j] == 1){
-          fresh++; 
-        }
-      }
-    }
-    if(fresh == 0) return 0;
 
-    while (!q.isEmpty()) {
-      boolean isrotted = false;
-      int size = q.size();
-      for(int k = 0; k < size; k++){
-        int[] item = q.poll();
-        int x = item[0];
-        int y = item[1];
-        for (int i = 0; i < 4; i++) {
-          int nrow = x + rowdir[i];
-          int ncol = y + coldir[i];
-          if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && !isvisited[nrow][ncol] && grid[nrow][ncol] == 1) {
-            q.offer(new int[] { nrow, ncol });
-            grid[nrow][ncol] = 2;
-            isrotted = true;
-            fresh--;
-            isvisited[nrow][ncol] = true;
-          }
+        if (fresh == 0)
+            return 0;
+
+        while (!q.isEmpty()) {
+            boolean flag = false;
+            int size = q.size();
+            
+            for (int i = 0; i < size; i++) {
+                int[] item = q.poll();
+                int x = item[0];
+                int y = item[1];
+                for (int[] moves : dir) {
+                    int nx = x + moves[0];
+                    int ny = y + moves[1];
+
+                    if (nx >= 0 && ny >= 0 && nx < grid.length && ny < grid[0].length && grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        fresh--;
+                        flag = true;
+                        q.offer(new int[] { nx, ny });
+
+                    }
+                }
+
+            }
+            if(flag){
+                time++;
+            }
+
         }
-      }
-    if(isrotted) time++;
+        if (fresh != 0)
+            return -1;
+
+        return time;
     }
-    if(fresh > 0) return -1;
-  return time;
-  }
+
 }
