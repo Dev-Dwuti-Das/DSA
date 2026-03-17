54937d74bd64b0f4fb1c0f2cd3fa3700cef8e60c
Dev Dwuti Das <devdwuti@gmail.com>
2026-03-17 20:32:15 +0530

    Time: 12 ms (91.31%) | Memory: 51.9 MB (61.79%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int countPaths(int n, int[][] roads) {
+
+        int MOD = (int)1e9 + 7;
+
+        List<List<int[]>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] e : roads) {
+            adj.get(e[0]).add(new int[]{e[1], e[2]});
+            adj.get(e[1]).add(new int[]{e[0], e[2]});
+        }
+
+        PriorityQueue<long[]> pq = new PriorityQueue<>(
+            (a, b) -> Long.compare(a[0], b[0])
+        );
+
+        long[] dist = new long[n];
+        long[] ways = new long[n];
+
+        Arrays.fill(dist, Long.MAX_VALUE);
+
+        dist[0] = 0;
+        ways[0] = 1;
+
+        pq.offer(new long[]{0, 0});
+
+        while(!pq.isEmpty()){
+            long[] item = pq.poll();
+            long cost = item[0];
+            int node = (int) item[1];   // ✅ FIX
+
+            if(cost > dist[node]) continue;
+
+            for(int[] nei : adj.get(node)){   // ✅ FIX
+                int next = nei[0];
+                int weight = nei[1];
+
+                long newDist = cost + weight;
+
+                if(newDist < dist[next]){
+                    dist[next] = newDist;
+                    ways[next] = ways[node];
+                    pq.offer(new long[]{newDist, next});  // ✅ FIX
+                }
+                else if(newDist == dist[next]){
+                    ways[next] = (ways[next] + ways[node]) % MOD;
+                }
+            }
+        }
+
+        return (int)(ways[n - 1] % MOD);
+    }
+}
