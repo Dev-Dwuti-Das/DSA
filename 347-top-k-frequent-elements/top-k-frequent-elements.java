518999f88c0f65971b19ab55136e22bcdc808080
Dev Dwuti Das <devdwuti@gmail.com>
2026-04-24 10:54:55 +0530

    Time: 11 ms (96.31%) | Memory: 47.4 MB (82.13%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,29 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map <Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
-        }
-         List<Integer>[] ls = new ArrayList[nums.length + 1];
-        for(Integer C : map.keySet()){
-            int freq = map.get(C);
-            if(ls[freq] == null){
-                ls[freq] = new ArrayList<>();
-            }
-                ls[freq].add(C);
-        }
+      Map <Integer, Integer> map = new HashMap<>();
+      for(int val : nums){
+        map.put(val, map.getOrDefault(val, 0) + 1);
+      }
+    
+    List<Integer>[] buckets = new ArrayList[nums.length + 1]; //list of arraylist
+    for(int keys : map.keySet()){
+      int freq = map.get(keys);
+      if(buckets[freq] == null){
+        buckets[freq] = new ArrayList<>();
+      }
+      buckets[freq].add(keys);
+    }
 
-        ArrayList <Integer> answer = new ArrayList<>();
-        for(int i = ls.length-1; i > 0; i--){
-            if(ls[i] != null){
-                answer.addAll(ls[i]);
-            }
-        }
+    List<Integer> res = new ArrayList<>();
+    for(int i = buckets.length-1; i >= 0; i--){
+      if(res.size() >= k) break;
+      if(buckets[i] != null){
+        res.addAll(buckets[i]);
+      }
+    }
+    int[] final_result = new int[k];
 
-        int[] res = new int[k];
-        for(int i = 0; i < k; i++){
-            res[i] = answer.get(i);
-        }
-    return res;
+    for(int j = 0; j < res.size(); j++){
+      final_result[j] = res.get(j);
+    }
+
+    return final_result;
+
     }
 }
