68181fb2150447550fe763b83793a5b516ef049a
Dev Dwuti Das <devdwuti@gmail.com>
2026-04-28 17:04:40 +0530

    Time: 4 ms (7.30%) | Memory: 64.9 MB (86.35%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,27 +1,27 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int[] result = new int[nums.length];
-        int[] prefix = new int[nums.length];
-        prefix[0] = nums[0];
-        int[] suffix = new int[nums.length];
-        suffix[nums.length-1] = nums[nums.length-1];
-        for(int i = 1; i < nums.length; i++){
-            prefix[i] = prefix[i-1] * nums[i]; 
-        }
-        for(int i = nums.length-2; i > 0 ; i--){
-            suffix[i] = suffix[i+1] * nums[i]; 
-        }
-        for(int i = 0; i < nums.length; i++){
-            if(i == 0){
-                result[i] = suffix[i+1]; 
-            }else if(i == nums.length-1){
-                result[nums.length-1] = prefix[i-1];
+  public int[] productExceptSelf(int[] nums) {
+    int[] prefix = new int[nums.length];
+    prefix[0] = nums[0];
+    int[] res = new int[nums.length];
+    int[] sufix = new int[nums.length];
+    sufix[nums.length - 1] = nums[nums.length - 1];
 
-            }else{
-            result[i] = prefix[i-1] * suffix[i+1]; 
-            }
-            
-        }
-    return result;
+    for (int i = 1; i < nums.length; i++) {
+      prefix[i] = prefix[i - 1] * nums[i];
+      sufix[nums.length - i - 1] = sufix[nums.length - i] * nums[nums.length - i - 1];
     }
+
+    for (int i = 0; i < nums.length; i++) {
+      if(i == 0){
+        res[i] = sufix[i+1];
+      }
+      else if(i == nums.length-1){
+        res[i] = prefix[i-1];
+      }
+      else{
+        res[i] = prefix[i - 1] * sufix[i + 1];
+      }
+    }
+   return res;
+  }
 }
