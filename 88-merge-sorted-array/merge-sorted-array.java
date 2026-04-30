a526bd99f1329b931ae5a86eaf666e4964a60009
Dev Dwuti Das <devdwuti@gmail.com>
2026-04-30 21:09:27 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (26.82%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,34 +1,35 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] temp = new int[m+n];
-        int k = 0;
-        int i = 0;
-        int j = 0;
-        while(i < m && j < n){
-            if(nums1[i] < nums2[j]){
-                temp[k] = nums1[i];
-                i++;
-            }else{
-                temp[k] = nums2[j];
-                j++;
-                
-            }
-            k++;
-        }
-        while(i < m){
-            temp[k] = nums1[i];
-            i++;
-            k++;
-        }
-        while(j < n){
-            temp[k] = nums2[j];
-            j++;
-            k++;
-        }
-        for(int p = 0; p < m+n; p++){
-            nums1[p] = temp[p];
-        }
-    
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int p1 = 0;
+    int k1 = 0;
+    int p2 = 0;
+    int[] temp = new int[m + n];
+    while (p1 < m && p2 < n) {
+      if (nums1[p1] < nums2[p2]) {
+        temp[k1] = nums1[p1];
+        k1++;
+        p1++;
+      } else {
+        temp[k1] = nums2[p2];
+        k1++;
+        p2++;
+      }
     }
-    
+
+    while (p2 < n) {
+      temp[k1] = nums2[p2];
+      k1++;
+      p2++;
+    }
+
+    while (p1 < m) {
+      temp[k1] = nums1[p1];
+      k1++;
+      p1++;
+    }
+
+    for (int i = 0; i < m + n; i++) {
+      nums1[i] = temp[i];
+    }
+  }
 }
