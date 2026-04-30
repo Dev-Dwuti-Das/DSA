600e26fab33279184c1bf5cc96fad064deaec811
Dev Dwuti Das <devdwuti@gmail.com>
2026-04-30 12:20:59 +0530

    Time: 33 ms (26.50%) | Memory: 46.9 MB (61.85%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,58 +1,52 @@
 class Solution {
+  int[] parents;
+  int[] size;
+  int count = 0;
+  public int removeStones(int[][] stones) {
+    int n = stones.length;
+    parents = new int[n];
+    size = new int[n];
 
-    int[] parent;
-    int[] size;
-
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
+    for (int i = 0; i < n; i++) {
+      parents[i] = i;
+      size[i] = 1; //galti
     }
 
-    void union(int a, int b) {
-        int pa = find(a);
-        int pb = find(b);
-        if (pa == pb) return;
-
-        parent[pb] = pa;
-        size[pa] += size[pb];
+    for(int i = 0; i < n; i++){
+      for(int j = i + 1; j < n; j++){
+        if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
+          union(find(i), find(j));
+        }
+      }
+    }
+    Map<Integer , Integer> map = new HashMap<>(); 
+    for(int i = 0; i < n; i++){
+       map.put(find(i), map.getOrDefault(find(i), 0)+1);
     }
 
-    public int removeStones(int[][] stones) {
-        int n = stones.length;
-
-        parent = new int[n];
-        size = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-
-        // your logic: union if same row or column
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
-                    union(i, j);
-                }
-            }
-        }
-
-        // count component sizes
-        java.util.HashMap<Integer, Integer> map = new java.util.HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            int root = find(i);
-            map.put(root, map.getOrDefault(root, 0) + 1);
-        }
-
-        // sum(size - 1)
-        int ans = 0;
-        for (int val : map.values()) {
-            ans += (val - 1);
-        }
-
-        return ans;
+    for(int val : map.values()){
+      count += val-1;
     }
+
+  return count;
+  }
+
+  public int find(int u) {
+    if (parents[u] == u) return u;
+    parents[u] = find(parents[u]);
+    return parents[u];
+  }
+
+  public void union(int u, int v) {
+    if (u == v)
+      return; //galti
+    if (size[u] > size[v]) {
+      parents[v] = u;
+      size[u] += size[v];
+    } else {
+      parents[u] = v;
+      size[v] += size[u];
+    }
+  }
 }
+
