ef6f66dac8e420d7a2be30098714e6b6e67dad2e
Dev Dwuti Das <devdwuti@gmail.com>
2026-05-04 14:30:32 +0530

    Time: 3 ms (33.80%) | Memory: 45.6 MB (67.24%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,29 +1,20 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] cand, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        int sum = target;
-        helper(cand, target, 0, sum, res, temp);
-        return res;
+  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> temp = new ArrayList<>();
+    helper(0, temp, res, candidates, target);
+    return res;
+  }
+
+  public void helper(int i, List<Integer> temp, List<List<Integer>> res, int[] cand, int target) {
+    if(i >= cand.length || target < 0) return;
+    if (i < cand.length && target == 0){
+     res.add(new ArrayList<>(temp));
+     return;
     }
-
-    public void helper(int[] cand, int target, int i, int sum, List<List<Integer>> res, List<Integer> temp) {
-        if (i > cand.length - 1) {
-            if (sum == 0) {
-                res.add(new ArrayList<>(temp));
-                return;
-            }
-            return;
-        }
-
-        if (sum < 0) return;
-        temp.add(cand[i]);
-        sum -= cand[i];
-        helper(cand, target, i, sum, res, temp);
-
-        temp.remove(temp.size() - 1);
-        sum += cand[i];
-
-        helper(cand, target, i + 1, sum, res, temp);
-    }
+    temp.add(cand[i]);
+    helper(i, temp, res, cand, target - cand[i]);
+    temp.remove(temp.size() - 1);
+    helper(i + 1, temp, res, cand, target);
+  }
 }
