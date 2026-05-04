3c38c3ae072167df07d0972c45c3e3829334e9a0
Dev Dwuti Das <devdwuti@gmail.com>
2026-05-04 11:17:05 +0530

    Time: 1 ms (88.85%) | Memory: 44.4 MB (20.69%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,24 +1,19 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
-        List<Integer> ls = new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
-        csubset(nums, 0, ls, res, n, set);
-        return res;
-    }
+  public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> temp = new ArrayList<>();
+    helper(0, res, temp, nums);
+    return res;
+  }
 
-    public void csubset(int[] nums, int i, List<Integer> ls, List<List<Integer>> res, int n, Set<List<Integer>> set){
-        if(i >= n){
-            if(set.add(new ArrayList<>(ls))){
-                res.add(new ArrayList<>(ls));
-            }
-            return;
-        }
-        ls.add(nums[i]);
-        csubset(nums, i+1, ls, res, n, set);
-        ls.remove(ls.size() - 1);
-        csubset(nums, i+1, ls, res, n, set);
+  public void helper(int i, List<List<Integer>> res, List<Integer> temp, int[] nums) {
+    if (i >= nums.length){
+      res.add(new ArrayList<>(temp));
+      return;
     }
-
+    temp.add(nums[i]);
+    helper(i + 1, res, temp, nums);
+    temp.remove(temp.size() - 1);
+    helper(i + 1, res, temp, nums);
+  }
 }
