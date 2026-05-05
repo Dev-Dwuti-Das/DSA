40d91e0a1052813441729bbdab3dd8696ab07311
Dev Dwuti Das <devdwuti@gmail.com>
2026-05-05 11:58:53 +0530

    Time: 2 ms (72.76%) | Memory: 44.8 MB (34.32%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,27 +1,22 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-    StringBuilder sb = new StringBuilder();
-    List<String> res = new ArrayList<>();
-    backtrack(n, sb, 0, 0, res);
-    return res;
+      List<String> res = new ArrayList<>();
+      helper(n, res, 0, 0, "");
+      return res;
     }
 
-    public void backtrack(int n, StringBuilder sb, int open, int close, List<String> res){
-        if(sb.length() ==  2*n){
-            res.add(sb.toString());
-            return;
-        }
+    public void helper(int n, List<String> res, int open, int close, String st){
+      if(st.length() == 2*n){
+        res.add(st);
+      }
 
-        if(open < n){
-            sb.append("(");
-            backtrack(n, sb, open+1, close, res);
-            sb.deleteCharAt(sb.length()-1);
-        }
+      if(open < n){
+        helper(n, res, open+1, close, st+"(");
+      }
 
-        if(close < open){
-            sb.append(")");
-            backtrack(n, sb, open, close+1, res);
-            sb.deleteCharAt(sb.length()-1);
-        }
+      if(close < open){
+        helper(n, res, open, close+1, st+")");
+      }
+
     }
 }
