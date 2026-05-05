8cd49ebc82c119c3424c82ca157609e5a5363045
Dev Dwuti Das <devdwuti@gmail.com>
2026-05-05 11:07:55 +0530

    Time: 1 ms (86.29%) | Memory: 45.3 MB (78.43%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,31 +1,24 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        boolean[] bool = new boolean[nums.length];
-        ArrayList<Integer> temp = new ArrayList<>();
-        ArrayList<List<Integer>> res= new ArrayList<>();
-        helper(nums, bool, temp, res);
-        return res;
-
+      List<Integer> temp = new ArrayList<>();
+      List<List<Integer>> res = new ArrayList<>();
+      boolean[] visited = new boolean[nums.length];
+      helper(nums, temp, res, visited);
+      return res;
     }
 
-    public void helper(int[] nums, boolean[] bool, ArrayList<Integer> temp, ArrayList<List<Integer>> res){
-            if( temp.size() == nums.length){
-                res.add(new ArrayList<>(temp));
-                return;
-            }
-
-            for(int j = 0; j < nums.length; j++){
-                if(bool[j]){    //bhara hua h to 
-                continue;
-                }
-                //khali
-                bool[j] = true;
-                temp.add(nums[j]);
-                helper(nums, bool, temp, res);
-                bool[j] = false;
-                temp.remove(temp.size()-1);
-            }
-
-
+    public void helper(int[] nums, List<Integer> temp, List<List<Integer>> res, boolean[] visited){
+      if(temp.size() == nums.length){
+        res.add(new ArrayList<>(temp));
+        return;
+      }
+      for(int j = 0; j < nums.length; j++){
+        if(visited[j]) continue;
+        visited[j] =  true;
+        temp.add(nums[j]);
+        helper(nums, temp, res, visited);
+        temp.remove(temp.size()-1);
+        visited[j] =  false;
+      }
     }
 }
