4d10122edde00c74c8428355b2dba907ebed76f8
Dev Dwuti Das <devdwuti@gmail.com>
2026-05-12 21:07:17 +0530

    Time: 750 ms (7.95%) | Memory: 48.1 MB (6.26%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,24 +1,39 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int r = 0;
-        List <Integer> ls = new ArrayList<>();
-        int[] p_count = new int[26];
-        int[] s_count = new int[26];
-        for(char c : p.toCharArray()){
-            p_count[ c - 'a']++;
-        }
+      int left = 0;
+      int right = p.length() - 1;
+      List<Integer> res = new ArrayList<>();
 
-        while(r < s.length()){
-            s_count[s.charAt(r) - 'a']++;
-            if(r >= p.length()){
-                s_count[s.charAt(r - p.length()) - 'a']--;
-            }
-            if(Arrays.equals(s_count,p_count)){
-                ls.add(r - p.length() + 1);
-            }
-            r++;
+      while(right < s.length()){
+        if(anagram(left, right, s, p)){
+          res.add(left);
         }
-        return ls;
-        
+        left++;
+        right++;
+      } 
+      return res;
+    }
+    // public boolean anagram(int left, int right, String s, String p){
+    //   String sub = s.substring(left, right+1);
+    //   char[] arry1 = sub.toCharArray();
+    //   Arrays.sort(arry1);
+    //   String sorted_s = new String(arry1);  
+    //   char[] arry2 = p.toCharArray();
+    //   Arrays.sort(arry2);
+    //   String sorted_p = new String(arry2);  
+    //   return sorted_s.equals(sorted_p);
+    // }
+    public boolean anagram(int left, int right, String s, String p){
+      int[] freq = new int[26];
+      for(int i = left ; i <= right; i++){
+        freq[s.charAt(i) - 'a']++;
+      }
+      for(char c : p.toCharArray()){
+        freq[c - 'a']--;
+      }
+      for(int x : freq){
+        if(x != 0) return false;
+      }
+      return true;
     }
 }
