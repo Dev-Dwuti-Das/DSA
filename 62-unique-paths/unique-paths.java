10eac581a99750dc58eb4daba9feab7e2588e58d
Dev Dwuti Das <devdwuti@gmail.com>
2026-05-14 10:22:07 +0530

    Time: 1 ms (13.25%) | Memory: 42.2 MB (46.55%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-      int count = 0;
-      int[][] dp = new int[m][n];
-      for(int i = 0; i < m; i++){
-        Arrays.fill(dp[i], -1);
+  public int uniquePaths(int m, int n) {
+    int count = 0;
+    int[][] dp = new int[m][n];
+    // for(int i = 0; i < m; i++){
+    //   Arrays.fill(dp[i], -1);
+    // }
+
+    dp[m - 1][n - 1] = 1;
+    for (int i = m - 1; i >= 0; i--) {
+      for (int j = n - 1; j >= 0; j--) {
+        int right = 0;
+        int down = 0;
+        if(i == m-1 && j == n-1) continue;
+        if(j + 1 < n) right = dp[i][j + 1];
+        if(i + 1 < m) down = dp[i+1][j];
+        dp[i][j] = right + down;
       }
-      return helper(0, 0, m, n, dp);
     }
+    return dp[0][0];
+  }
 
-    public int helper(int row, int col, int m, int n, int[][] dp){
-      if(row == m-1 && col == n-1) return 1;
-      if(row < 0 || row >= m || col < 0 || col >= n) return 0;
-
-      if(dp[row][col] != -1) return dp[row][col];
-
-      int right = helper(row, col+1, m, n, dp);
-      int down = helper(row + 1, col, m, n, dp);
-
-      int sum = right + down;
-      return dp[row][col] = sum;
-    }
 }
