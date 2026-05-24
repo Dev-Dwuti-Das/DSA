23f011ef9a38ee2ed1857f0c5f9946e9b4fd5a64
Dev Dwuti Das <devdwuti@gmail.com>
2026-05-24 18:33:25 +0530

    Time: 22 ms (29.26%) | Memory: 47.6 MB (92.46%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,23 +1,33 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        Arrays.sort(piles);
-        int max = piles[piles.length-1];
-        int min = 1;
 
-        while(min < max){
-            int var_h = h;
-            int mid = (max+min)/2; //6
-            for(int i = 0; i < piles.length; i++){
-                int time_taken = (piles[i] + mid - 1)/mid;
-                var_h = var_h - time_taken;
-            }
-            if(var_h >= 0){
-                max = mid;
-            }
-            else{
-                min = mid+1;
-            }
+      int[] temp = piles.clone();
+      Arrays.sort(piles);
+      int min = 1;
+      int max = piles[piles.length-1];
+
+      while(min < max){
+        int mid = (min+max)/2;
+        int val = isvalid(temp, mid, h);
+
+        if( val > h){ //taking more time
+          min = mid+1; 
+        }else{ //taking less time
+          max = mid;
         }
-    return min;
+      }
+      return min;
+    }
+
+    public int isvalid(int[] nums, int mid, int h){
+      int time_taken = 0;
+      for(int i = 0; i < nums.length; i++){
+          if(nums[i]%mid == 0){
+            time_taken += nums[i]/mid;
+          }else{
+            time_taken += nums[i]/mid + 1;
+          }
+      }
+      return time_taken;
     }
 }
