0a03c68d9a6561cab0e835509c4f72c1e3f39772
Dev Dwuti Das <devdwuti@gmail.com>
2026-05-24 13:31:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (45.35%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,34 +1,30 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int min = 0;
-        int max = nums.length - 1;
+  public int search(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
 
-        if (nums.length == 1 && target == nums[0]) {
-            return 0;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+
+      if (nums[mid] == target)
+        return mid;
+
+      if (nums[left] <= nums[mid]) { // left sorted
+
+        if (nums[left] <= target && target < nums[mid]) { // element here
+          right = mid - 1;
+        } else {
+          left = mid + 1;
         }
-
-        while (min <= max) {
-            int mid = (min + max) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            if (nums[min] <= nums[mid]) {
-                if (target < nums[mid] && target >= nums[min]) {
-                    max = mid - 1;
-                } else {
-                    min = mid + 1;
-                }
-            } else {
-                if (target > nums[mid] && target <= nums[max]) {
-                    min = mid + 1;
-                } else {
-                    max = mid - 1;
-
-                }
-            }
+      } else {// right sorted
+        if (nums[mid] < target && target <= nums[right]) { //element here
+          left = mid + 1;
+        } else {
+          right = mid - 1;
         }
-        return -1;
+      }
     }
+
+    return -1;
+  }
 }
