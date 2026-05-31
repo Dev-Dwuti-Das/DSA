d47933dce83dcefa3bb9425a2b58f0d4344ae85e
Dev Dwuti Das <devdwuti@gmail.com>
2026-05-31 17:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (58.06%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,40 +1,41 @@
 class MyQueue {
-    Stack<Integer> input;
-    Stack<Integer> output;
+  Stack <Integer> input;
+  Stack <Integer> output;
 
     public MyQueue() {
-        input = new Stack<>();
-        output = new Stack<>();
+      input = new Stack<>();
+      output = new Stack<>();
     }
-
+    
     public void push(int x) {
-        input.push(x);
+      input.push(x);
     }
-
+    
     public int pop() {
-        if (output.isEmpty()) {
-            while (!input.isEmpty()) {
-                output.push(input.pop());
-            }
+      if(output.isEmpty()){
+        while(!input.isEmpty()){
+          output.push(input.pop());
         }
-        return output.pop();
+      }
+      return output.pop();
     }
-
+    
     public int peek() {
-        if (output.isEmpty()) {
-            while (!input.isEmpty()) {
-                output.push(input.pop());
-            }
+      if(output.isEmpty()){
+        while(!input.isEmpty()){
+          output.push(input.pop());
         }
-        return output.peek();
+      }
+    return output.peek();        
     }
-
+    
     public boolean empty() {
-        if (output.isEmpty() && input.isEmpty()) {
-            return true;
-        } else {
-            return false;
+      if(output.isEmpty()){
+        while(!input.isEmpty()){
+          output.push(input.pop());
         }
+      }
+      return output.isEmpty();
     }
 }
 
