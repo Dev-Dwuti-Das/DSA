22d5b7e5efdff1fb2b138310ba35e335e297f614
Dev Dwuti Das <devdwuti@gmail.com>
2026-05-31 16:17:27 +0530

    Time: 6 ms (20.23%) | Memory: 47.1 MB (28.15%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,32 +1,25 @@
 class Solution {
-    public int[] asteroidCollision(int[] ast) {
-        Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < ast.length; i++) {
-            boolean destroyed = false;
-            while (!st.isEmpty() && ast[i] < 0 && st.peek() > 0) {
-                int diff = ast[i] + st.peek();
-                if(diff < 0){
-                    st.pop();
-                }
-                else if ( diff == 0){
-                    destroyed = true;
-                    st.pop();
-                    break;
-                }
-                else{
-                    destroyed = true;
-                    break;
-                }
-            }
-            if(!destroyed){
-                st.push(ast[i]);
-            }
-            
+  public int[] asteroidCollision(int[] asteroids) {
+    Stack<Integer> st = new Stack<>();
+    for (int val : asteroids) {
+
+      while (!st.isEmpty() && st.peek() > 0 && val < 0 && Math.abs(val) > st.peek()) {
+        st.pop();
+      }
+      if (!st.isEmpty() && st.peek() > 0 && val < 0) {
+        if (st.peek() == Math.abs(val)) {
+          st.pop();
         }
-        int res[] = new int[st.size()];
-        for (int i = 0; i < st.size(); i++) {
-            res[i] = st.get(i);
-        }
-        return res;
+      }else{
+        st.add(val);
+      }
     }
+
+    int[] res = new int[st.size()];
+    for (int i = st.size() - 1; i >= 0; i--) {
+      res[i] = st.pop();
+    }
+    return res;
+
+  }
 }
