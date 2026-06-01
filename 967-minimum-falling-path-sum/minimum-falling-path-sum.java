37da1a35c648de9998ee18ea0ed77300c8c8193d
Dev Dwuti Das <devdwuti@gmail.com>
2026-06-01 11:19:28 +0530

    Time: 1 ms (99.97%) | Memory: 47.4 MB (16.85%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -2,11 +2,11 @@ class Solution {
   public int minFallingPathSum(int[][] matrix) {
     int res = Integer.MAX_VALUE;
 
-    Integer[][] dp = new Integer[matrix.length][matrix[0].length];
+    int[][] dp = new int[matrix.length][matrix[0].length];
 
-    // for (int i = 0; i < dp.length; i++) {
-    //   Arrays.fill(dp[i], (int)1e9);
-    // }
+    for (int i = 0; i < dp.length; i++) {
+      Arrays.fill(dp[i], (int)1e9);
+    }
 
     for (int k = 0; k < matrix[0].length; k++) {
       res = Math.min(res, helper(matrix, dp, 0, k));
@@ -15,7 +15,7 @@ class Solution {
     return res;
   }
 
-  public int helper(int[][] mat, Integer[][] dp, int i, int j) {
+  public int helper(int[][] mat, int[][] dp, int i, int j) {
 
     if (i >= mat.length || j >= mat[0].length || j < 0)
       return Integer.MAX_VALUE;
@@ -23,7 +23,7 @@ class Solution {
     if (i == mat.length - 1)
       return mat[i][j];
 
-    if(dp[i][j] != null) return dp[i][j];
+    if(dp[i][j] != (int)1e9) return dp[i][j];
 
     int down = helper(mat, dp, i + 1, j);
     int down_left = helper(mat, dp, i + 1, j - 1);
