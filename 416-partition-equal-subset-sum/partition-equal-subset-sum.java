6802f6cc0e55fb34ec15b5e82bb9dc32cc52c080
Dev Dwuti Das <devdwuti@gmail.com>
2026-06-03 16:58:59 +0530

    Time: 112 ms (8.89%) | Memory: 75.5 MB (19.72%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,12 +1,13 @@
 class Solution {
   public boolean canPartition(int[] nums) {
-    int sum = 0; 
+    int sum = 0;
     for (int i = 0; i < nums.length; i++) {
       sum += nums[i];
     }
-    if(sum % 2 != 0) return false;
+    if (sum % 2 != 0)
+      return false;
     int temp = sum / 2;
-    Boolean[][] dp = new Boolean[nums.length+1][temp+1];
+    Boolean[][] dp = new Boolean[nums.length + 1][temp + 1];
     return helper(0, temp, nums, dp);
   }
 
@@ -15,13 +16,14 @@ class Solution {
       return false;
     if (temp == 0)
       return true;
-    
-    if(dp[i][temp] != null) return dp[i][temp]; 
+
+    if (dp[i][temp] != null)
+      return dp[i][temp];
     Boolean taken = false;
     if (nums[i] <= temp) {
       taken = helper(i + 1, temp - nums[i], nums, dp);
     }
-    Boolean not = helper(i + 1, temp , nums, dp);
+    Boolean not = helper(i + 1, temp, nums, dp);
 
     return dp[i][temp] = not || taken;
   }
