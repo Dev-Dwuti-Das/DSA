9620080404e70f7d837b09a4418a5c5573e49394
Dev Dwuti Das <devdwuti@gmail.com>
2026-06-08 10:06:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (91.85%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -16,27 +16,18 @@
 class Solution {
   public List<Integer> inorderTraversal(TreeNode root) {
     List<Integer> res = new ArrayList<>();
-    traversal(root, res);
+    if (root == null)
+      return new ArrayList<>();
+    dfs(root, res);
     return res;
   }
 
-  public void traversal(TreeNode root, List<Integer> res) {
-    TreeNode curr = root;
-    while (curr != null) {
-      if (curr.left == null) {
-        res.add(curr.val);
-        curr = curr.right;
-      } else {
-        TreeNode leftchild = curr.left;
-        while (leftchild.right != null) {
-          leftchild = leftchild.right;
-        }
-        leftchild.right = curr;
+  public void dfs(TreeNode root, List<Integer> res) {
+    if (root == null)
+      return;
 
-        TreeNode temp = curr.left;
-        curr.left = null;
-        curr = temp;
-      }
-    }
+    dfs(root.left, res);
+    res.add(root.val);
+    dfs(root.right, res);
   }
 }
