41a993460aff1cba5c0b0787e56e80662877206e
Dev Dwuti Das <devdwuti@gmail.com>
2026-06-09 14:01:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (31.91%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,21 +15,17 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-    
     return dfs(p, q);
-        
     }
 
 
     public boolean dfs(TreeNode p, TreeNode q){
 
       if(p == null && q == null) return true;
-      if(p == null || q == null) return false;
-      
 
-      if(p.val != q.val){
-        return false;
-      }
+      if(p == null || q == null || p.val != q.val) return false;
+      
+     
 
       boolean left = dfs(p.left, q.left);
       if(!left) return false;
