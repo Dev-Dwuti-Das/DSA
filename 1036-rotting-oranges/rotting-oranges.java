9f44d9ddc6b3997984e16edf2ec313c18a40926d
Dev Dwuti Das <devdwuti@gmail.com>
2026-06-23 13:50:34 +0530

    Time: 2 ms (90.75%) | Memory: 44.2 MB (56.01%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,56 +1,54 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        int fresh = 0;
-        int time = 0;
-        int[][] dir = {
-                { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 }
-        };
-        Queue<int[]> q = new LinkedList<>();
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 2) {
-                    q.offer(new int[] { i, j });
-                }
-                if (grid[i][j] == 1) {
-                    fresh++;
-                }
-            }
+  int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+  public int orangesRotting(int[][] grid) {
+    int time = 0;
+    int fresh = 0;
+    int rotted = 0;
+    Queue<int[]> q = new LinkedList<>();
+
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] == 1) {
+          fresh++;
         }
-
-        if (fresh == 0)
-            return 0;
-
-        while (!q.isEmpty()) {
-            boolean flag = false;
-            int size = q.size();
-            
-            for (int i = 0; i < size; i++) {
-                int[] item = q.poll();
-                int x = item[0];
-                int y = item[1];
-                for (int[] moves : dir) {
-                    int nx = x + moves[0];
-                    int ny = y + moves[1];
-
-                    if (nx >= 0 && ny >= 0 && nx < grid.length && ny < grid[0].length && grid[nx][ny] == 1) {
-                        grid[nx][ny] = 2;
-                        fresh--;
-                        flag = true;
-                        q.offer(new int[] { nx, ny });
-
-                    }
-                }
-
-            }
-            if(flag){
-                time++;
-            }
-
+        if (grid[i][j] == 2) {
+          rotted++;
+          q.offer(new int[] { i, j });
         }
-        if (fresh != 0)
-            return -1;
-
-        return time;
+      }
     }
+    if (fresh + rotted == 0)
+      return 0;
+    while (!q.isEmpty()) {
+      boolean cur_rotted = false;
 
+      int size = q.size();
+      for (int i = 0; i < size; i++) {
+        int[] item = q.poll();
+        int x = item[0];
+        int y = item[1];
+        if (grid[x][y] == 1) {
+          cur_rotted = true;
+          fresh--;
+        }
+        grid[x][y] = 2;
+
+        for (int[] nei : dir) {
+          int nr = x + nei[0];
+          int nc = y + nei[1];
+          if (nr < grid.length && nc < grid[0].length && nr >= 0 && nc >= 0 && grid[nr][nc] == 1) {
+            q.offer(new int[] { nr, nc });
+          }
+        }
+      }
+      if (cur_rotted)
+        time++;
+    }
+    if (fresh != 0)
+      return -1;
+    return time;
+  }
 }
+
+//&& image[nr][nc] != color
