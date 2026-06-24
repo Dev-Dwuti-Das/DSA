a774fa7950db76ed16ae0edce1e928c495b5d490
Dev Dwuti Das <devdwuti@gmail.com>
2026-06-24 12:20:05 +0530

    Time: 2 ms (73.05%) | Memory: 47.7 MB (55.54%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,36 +1,43 @@
 class Solution {
+  int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 
-    //EASY QUESTION H KHUD SE KR
-    public void solve(char[][] board) {
+  public void solve(char[][] grid) {
+    boolean[][] visited = new boolean[grid.length][grid[0].length];
 
-        for (int i = 0; i < board.length; i++) {
-            dfs(i, 0, board);
-            dfs(i, board[0].length - 1, board);
-        }
-
-        for (int j = 0; j < board[0].length; j++) {
-            dfs(0, j, board);
-            dfs(board.length - 1, j, board);
-        }
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 'O')
-                    board[i][j] = 'X';
-                if (board[i][j] == '#')
-                    board[i][j] = 'O';
-            }
-        }
+    for (int i = 0; i < grid.length; i++) {
+      if (grid[i][0] == 'O') {
+        dfs(i, 0, grid, visited);
+      }
+      if (grid[i][grid[0].length - 1] == 'O') {
+        dfs(i, grid[0].length - 1, grid, visited);
+      }
     }
-
-    public void dfs(int i, int j, char[][] board) {
-        if (i >= board.length || j >= board[0].length || i < 0 || j < 0 || board[i][j] == 'X' || board[i][j] == '#') {
-            return;
-        }
-        board[i][j] = '#';
-        dfs(i + 1, j, board);
-        dfs(i - 1, j, board);
-        dfs(i, j - 1, board);
-        dfs(i, j + 1, board);
+    for (int j = 0; j < grid[0].length; j++) {
+      if (grid[0][j] == 'O') {
+        dfs(0, j, grid, visited);
+      }
+      if (grid[grid.length - 1][j] == 'O') {
+        dfs(grid.length - 1, j, grid, visited);
+      }
     }
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        if (!visited[i][j]) {
+          grid[i][j] = 'X';
+        }
+      }
+    }
+  }
 
+  public void dfs(int i, int j, char[][] grid, boolean[][] visited) {
+    if (i >= grid.length || j >= grid[0].length || i < 0 || j < 0 || visited[i][j] || grid[i][j] == 'X')
+      return;
+    visited[i][j] = true;
+    
+    for (int[] d : dir) {
+      int nr = i + d[0];
+      int nc = j + d[1];
+      dfs(nr, nc, grid, visited);
+    }
+  }
 }
