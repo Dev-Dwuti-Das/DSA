d8afa1a1e8e8b1d1c0fb01e4f0f6a7a8ebcf8f60
Dev Dwuti Das <devdwuti@gmail.com>
2026-06-24 11:53:16 +0530

    Time: 8 ms (70.28%) | Memory: 60.9 MB (36.52%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,35 +1,44 @@
 class Solution {
-    public int numEnclaves(int[][] grid) {
-        int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            dfs(i, 0, grid);
-            dfs(i, grid[0].length - 1, grid);
-        }
+  int total_1s = 0;
+  int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 
-        for (int j = 0; j < grid[0].length; j++) {
-            dfs(0, j, grid);
-            dfs(grid.length - 1, j, grid);
+  public int numEnclaves(int[][] grid) {
+    boolean[][] visited = new boolean[grid.length][grid[0].length];
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] == 1) {
+          total_1s++;
         }
-
-        for (int k = 0; k < grid.length; k++) {
-            for (int p = 0; p < grid[0].length; p++) {
-                if (grid[k][p] == 1) {
-                    count++;
-                }
-            }
-        }
-        return count;
+      }
     }
-
-    public void dfs(int i, int j, int[][] grid) {
-
-        if (i >= grid.length || j >= grid[0].length || i < 0 || j < 0 || grid[i][j] == 0 || grid[i][j] == -1) {
-            return;
-        }
-        grid[i][j] = -1;
-        dfs(i+1, j, grid);
-        dfs(i-1, j, grid);
-        dfs(i, j+1, grid);
-        dfs(i, j-1, grid);
+    for (int i = 0; i < grid.length; i++) {
+      if (grid[i][0] == 1) {
+        dfs(i, 0, grid, visited);
+      }
+      if (grid[i][grid[0].length - 1] == 1) {
+        dfs(i, grid[0].length - 1, grid, visited);
+      }
     }
+    for (int j = 0; j < grid[0].length; j++) {
+      if (grid[0][j] == 1) {
+        dfs(0, j, grid, visited);
+      }
+      if (grid[grid.length - 1][j] == 1) {
+        dfs(grid.length - 1, j, grid, visited);
+      }
+    }
+    return total_1s;
+  }
+
+  public void dfs(int i, int j, int[][] grid, boolean[][] visited) {
+    if (i >= grid.length || j >= grid[0].length || i < 0 || j < 0 || visited[i][j] || grid[i][j] == 0)
+      return;
+    total_1s--;
+    visited[i][j] = true;
+    for (int[] d : dir) {
+      int nr = i + d[0];
+      int nc = j + d[1];
+      dfs(nr, nc, grid, visited);
+    }
+  }
 }
