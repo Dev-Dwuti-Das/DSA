59d8992cb6c6f452acb46a69a9c61c734b359760
Dev Dwuti Das <devdwuti@gmail.com>
2026-06-24 11:19:29 +0530

    Time: 16 ms (26.69%) | Memory: 49.3 MB (34.10%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,46 +1,43 @@
 class Solution {
+  int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
   public int[][] updateMatrix(int[][] mat) {
-
-    int m = mat.length;
-    int n = mat[0].length;
-
+    int[][] res = new int[mat.length][mat[0].length];
+    int[][] visited = new int[mat.length][mat[0].length];
     Queue<int[]> q = new LinkedList<>();
 
-    // step 1: push all 0s
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
+    for (int i = 0; i < mat.length; i++) {
+      for (int j = 0; j < mat[0].length; j++) {
         if (mat[i][j] == 0) {
-          q.offer(new int[]{i, j});
-        } else {
-          mat[i][j] = -1; // mark unvisited 1
+          q.offer(new int[] { i, j, 0 });
+          visited[i][j] = 1;
         }
       }
     }
 
-    int[][] dir = {
-        {1,0},{-1,0},{0,1},{0,-1}
-    };
-
-    // step 2: BFS
     while (!q.isEmpty()) {
 
-      int[] item = q.poll();
-      int x = item[0];
-      int y = item[1];
+      int size = q.size();
+      for (int i = 0; i < size; i++) {
 
-      for (int[] moves : dir) {
+        int[] item = q.poll();
+        int x = item[0];
+        int y = item[1];
+        int dist = item[2];
+        
+        res[x][y] = dist;
 
-        int nx = x + moves[0];
-        int ny = y + moves[1];
+        for (int[] nei : dir) {
+          int nr = x + nei[0];
+          int nc = y + nei[1];
 
-        if (nx >= 0 && ny >= 0 && nx < m && ny < n && mat[nx][ny] == -1) {
-
-          mat[nx][ny] = mat[x][y] + 1;
-          q.offer(new int[]{nx, ny});
+          if (nr < mat.length && nc < mat[0].length && nr >= 0 && nc >= 0 && visited[nr][nc] == 0) {
+            visited[nr][nc] = 1;
+            q.offer(new int[] { nr, nc, dist + 1 });
+          }
         }
       }
     }
-
-    return mat;
+    return res;
   }
 }
