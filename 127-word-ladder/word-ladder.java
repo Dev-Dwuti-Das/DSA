c5f848f60455ca55a4ddb9c5d39fd09643801ecb
Dev Dwuti Das <devdwuti@gmail.com>
2026-06-25 16:08:18 +0530

    Time: 74 ms (60.92%) | Memory: 48.7 MB (36.34%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,45 +1,46 @@
 class pair {
-    String st;
-    int steps;
+  String st;
+  int level;
 
-    public pair(String st, int steps) {
-        this.st = st;
-        this.steps = steps;
-    }
+  pair(String st, int lvl) {
+    this.st = st;
+    this.level = lvl;
+  }
 }
 
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<pair> q = new LinkedList<>();
-        Set<String> set = new HashSet<>();
-        for (String st : wordList) {
-            set.add(st);
-        }
-        if (!set.contains(endWord))
-            return 0;
-        q.offer(new pair(beginWord, 1));
-
-        while (!q.isEmpty()) {
-            pair item = q.poll();
-            String word = item.st;
-            int steps = item.steps;
-            char[] word_char = word.toCharArray();
-            for (int i = 0; i < word.length(); i++) {
-                char original = word_char[i];
-                for (char ch = 'a'; ch <= 'z'; ch++) {
-                    word_char[i] = ch;
-                    String newstring = new String(word_char);
-                    if (newstring.equals(endWord)) {
-                        return steps + 1;
-                    }
-                    if (set.contains(newstring)) {
-                        q.offer(new pair(newstring, steps + 1));
-                        set.remove(newstring);
-                    }
-                }
-                word_char[i] = original;
-            }
-        }
-        return 0;
+  public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    Set<String> set = new HashSet<>();
+    Queue<pair> q = new LinkedList<>();
+    for (String st : wordList) {
+      set.add(st);
     }
+    q.offer(new pair(beginWord, 1));
+
+    while (!q.isEmpty()) {
+      int size = q.size();
+
+      for (int i = 0; i < size; i++) {
+        pair item = q.poll();
+        String word = item.st;
+        int lvl = item.level;
+        if (word.equals(endWord))
+          return lvl;
+
+        for (int j = 0; j < word.length(); j++) {
+          char[] original = word.toCharArray();
+          for (char ch = 'a'; ch <= 'z'; ch++) {
+            original[j] = ch;
+            String newstring = new String(original);
+            if (set.contains(newstring)) {
+              q.offer(new pair(newstring, lvl + 1));
+              set.remove(newstring);
+            }
+          }
+
+        }
+      }
+    }
+    return 0;
+  }
 }
