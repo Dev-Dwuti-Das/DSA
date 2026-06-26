553e4292173b3eefaa77904dfd623413bcbd83d6
Dev Dwuti Das <devdwuti@gmail.com>
2026-06-26 12:43:26 +0530

    Time: 8 ms (26.22%) | Memory: 47 MB (63.33%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,45 +1,39 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] edges) {
-        int V = numCourses;
+    public boolean canFinish(int V, int[][] box) {
+     int[] inedges = new int[V];
+      List<List<Integer>> adj = new ArrayList<>();
+      Queue<Integer> q = new LinkedList<>();
 
-        List<List<Integer>> adj = new ArrayList<>();
-        ArrayList<Integer> res = new ArrayList<>();
-
-        for (int k = 0; k < V; k++) {
-            adj.add(new ArrayList<>());
+      for(int i = 0; i < V; i++){
+        adj.add(new ArrayList<>());
+      }
+      for(int[] e : box){
+        adj.get(e[1]).add(e[0]);
+      }
+      for(int i = 0; i < V; i++){
+        for(int x : adj.get(i)){
+          inedges[x]++;
         }
+      }
 
-        for (int[] e : edges) {
-            adj.get(e[0]).add(e[1]);
+      for(int i = 0; i < V; i++){
+        if(inedges[i] == 0){
+          q.offer(i);
         }
+      }
 
-        int[] inorder = new int[V];
-        for (int i = 0; i < V; i++) {
-            for (int x : adj.get(i)) {
-                inorder[x]++;
-            }
+      int count = 0;
+      while(!q.isEmpty()){
+        int item = q.poll();
+        count++;
+
+        for(int nei : adj.get(item)){
+          inedges[nei]--;
+          if(inedges[nei] == 0) q.offer(nei);
         }
+      }
 
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < V; i++) {
-            if (inorder[i] == 0) {
-                q.offer(i);
-            }
-        }
-        
-        int count = 0;
-        while (!q.isEmpty()) {
-            int item = q.poll();
-            count++;
+    return count == V;
 
-            for (int x : adj.get(item)) {
-                inorder[x]--;
-                if (inorder[x] == 0) {
-                    q.offer(x);
-                }
-            }
-
-        }
-     return count == V;
     }
 }
