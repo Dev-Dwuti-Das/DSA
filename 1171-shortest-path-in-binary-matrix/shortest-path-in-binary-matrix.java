a98f7eb1c4ee7e20e5e5f53c7bfdb8834ae31402
Dev Dwuti Das <devdwuti@gmail.com>
2026-06-27 14:23:38 +0530

    Time: 17 ms (43.25%) | Memory: 48.1 MB (12.54%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,50 +1,41 @@
 class Solution {
-    public int shortestPathBinaryMatrix(int[][] grid) {
-        Queue<int[]> q = new LinkedList<>();
-        int n = grid.length;
-        int m = grid[0].length;
-        if(grid[0][0] == 1) return -1;
-        int[][] dist = new int[grid.length][grid[0].length];
-        for(int i = 0; i < grid.length; i++){
-            Arrays.fill(dist[i], (int)1e9);
+  int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 1 }, { -1, -1 }, { 1, -1 }, { -1, 1 } };
+
+  public int shortestPathBinaryMatrix(int[][] grid) {
+    int[][] visited = new int[grid.length][grid[0].length];
+    Queue<int[]> q = new LinkedList<>();
+    if (grid[0][0] == 1)
+      return -1;
+
+    q.offer(new int[] { 0, 0, 1 });
+
+    while (!q.isEmpty()) {
+
+      int size = q.size();
+      for (int i = 0; i < size; i++) {
+        int[] item = q.poll();
+        int x = item[0];
+        int y = item[1];
+        int dist = item[2];
+
+        if (x == grid.length - 1 && y == grid[0].length - 1) {
+          return dist;
         }
-        dist[0][0] = 0;
-        q.offer(new int[] { 0, 0, 0 });
-        int[][] dir = {
-                { -1, 0 },
-                { 1, 0 },
-                { 0, 1 },
-                { 0, -1 },
-                { 1, 1 },
-                { -1, -1 },
-                { -1, 1 },
-                { 1, -1 }
-        };
 
-        while (!q.isEmpty()) {
-            int[] item = q.poll();
-            int x = item[0];
-            int y = item[1];
-            int cost = item[2];
+        for (int[] d : dir) {
+          int nr = x + d[0];
+          int nc = y + d[1];
 
-            if (x == n-1 && y == m-1) {
-                return dist[x][y] + 1;
-            }
-
-            for(int i = 0; i < 8; i++){
-                int nx = x + dir[i][0];
-                int ny = y + dir[i][1];
-
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 0){
-                    if(dist[nx][ny] > cost + 1){
-                        dist[nx][ny] = cost + 1;
-                        q.offer(new int[]{nx,ny,cost + 1});
-                    }
-                }
-            }
-
+          if (nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == 0
+              && visited[nr][nc] == 0) {
+            visited[nr][nc] = 1;
+            q.offer(new int[] { nr, nc, dist + 1 });
+          }
         }
-    
-    return -1;
+      }
     }
+
+    return -1;
+
+  }
 }
