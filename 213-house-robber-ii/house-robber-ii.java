6cb66c8363ad9da1ab618815817ffaf5f5e81c1a
Dev Dwuti Das <devdwuti@gmail.com>
2026-07-18 06:49:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (61.09%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,22 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
-      if(nums.length <= 1) return nums[0];
+      if(nums.length == 1) return nums[0];
+
       int n = nums.length;
       int[] dp1 = new int[n];
-      Arrays.fill(dp1, -1);
       int[] dp2 = new int[n];
+      Arrays.fill(dp1, -1);
       Arrays.fill(dp2, -1);
-      return Math.max(helper1(1, nums, n-1, dp1), helper1(0, nums, n-2, dp2));
+      return Math.max(helper(n-1, 1, nums, dp1), helper(n-2, 0, nums, dp2));
     }
 
-    public int helper1(int i, int[] nums, int end, int[] dp1){  
+
+    public int helper(int i, int end, int[] nums, int[] dp){
       if(i == end) return nums[i];
-      if(i > end) return 0;
-      if(dp1[i] != -1) return dp1[i];
-      int path1 = nums[i] + helper1(i+2, nums, end, dp1);
-      int path2 = 0 + helper1(i+1, nums, end, dp1);
-      return dp1[i] = Math.max(path1, path2);
-    }
+      if(i < end) return 0;
 
-    
+      if(dp[i] != -1) return dp[i];
+
+      int take = nums[i] + helper(i - 2, end, nums, dp);
+      int not_take = 0 + helper(i - 1, end, nums, dp); 
+
+      return dp[i] = Math.max(take, not_take);
+    }
 }
