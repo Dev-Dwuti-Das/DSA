d1a178a50354e5f17358fb60278ccd635eff3b82
Dev Dwuti Das <devdwuti@gmail.com>
2026-07-18 11:37:43 +0530

    Time: 2 ms (2.13%) | Memory: 47.8 MB (94.42%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -31,22 +31,20 @@ class Solution {
     public int binsearch_left(int[] nums, int target){
       int left = 0;
       int right = nums.length-1;
-      int value = -1;
+      int val = -1;
 
       while(left <= right){
         int mid = (left + right)/2;
-
-        if(nums[mid] == target){
-          value = mid;
-          right = mid - 1;
-        }
-        else if(nums[mid] < target){
+        if(nums[mid] < target){
           left = mid + 1;
-        }else{
+        }else if(nums[mid] > target){
           right = mid - 1;
-
+        }else{
+          val = mid;
+          right = mid - 1;
         }
+
       }
-      return value;
+    return val;
     }
 }
