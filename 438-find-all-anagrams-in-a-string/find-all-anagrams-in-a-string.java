4c955620774de76b20b6aa4f49263150e9edfc65
Dev Dwuti Das <devdwuti@gmail.com>
2026-08-30 20:41:03 +0530

    Time: 9 ms (92.25%) | Memory: 47 MB (45.91%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,29 +1,28 @@
 class Solution {
-  public List<Integer> findAnagrams(String s, String p) {
-    int[] freq_p = new int[26];
-    int[] freq_s = new int[26];
-    List<Integer> res = new ArrayList<>();
+    public List<Integer> findAnagrams(String s, String p) {
+      int[] s_arr = new int[26];
+      int[] p_arr = new int[26];
+      List<Integer> res = new ArrayList<>();
+      if(p.length() > s.length()) return new ArrayList<>();
 
-    if(p.length() > s.length()) return new ArrayList<>();
-
-    for (char c : p.toCharArray()) {
-      freq_p[c - 'a']++;
-    }
-    int right = 0;
-    int left = 0;
-
-    while (right < s.length()) {
-      while (right - left + 1 <= p.length()) {
-        freq_s[s.charAt(right) - 'a']++;
-        right++;
+      for(char c : p.toCharArray()){
+        p_arr[c - 'a']++;
       }
-      if (Arrays.equals(freq_p, freq_s)) {
-        res.add(left);
-      }
-      freq_s[s.charAt(left) - 'a']--;
-      left++;
-    }
 
+      int l = 0;
+      int r = 0;
+
+      while(r < s.length()){
+        while(r - l < p.length()){
+          s_arr[s.charAt(r) - 'a']++;
+          r++;
+        }
+        if(Arrays.equals(s_arr, p_arr)){
+          res.add(l);
+        }
+        s_arr[s.charAt(l) - 'a']--;
+        l++;
+      }
     return res;
-  }
+    }
 }
